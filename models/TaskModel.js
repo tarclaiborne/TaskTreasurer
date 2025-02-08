5829408b16e853986b927d7734eb153b40b5bdb9
tarclaiborne <tarclaiborne@gmail.com>
2025-02-08 16:17:19 -0600

    Initial commit

---
+++ models/TaskModel.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose'); const taskSchema = new mongoose.Schema({ title: { type: String, required: true }, description: { type: String }, completed: { type: Boolean, default: false }, }, { timestamps: true }); const Task = mongoose.model('Task', taskSchema); module.exports = Task;
---
+++ server.js
@@ -0,0 +1,1 @@
+require('dotenv').config(); // Load environment variables const mongoose = require('mongoose'); // Corrected import // Connect to MongoDB mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, }) .then(() => console.log("MongoDB Connected")) .catch(err => console.error("Connection Error:", err));
